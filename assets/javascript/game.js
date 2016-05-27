68f07dcfbfa4795cdaf88d9659995f55aad52ebe
Jennine Punzone <jennine@Jennine-Punzones-MacBook-Pro.local>
2016-05-27 13:52:18 -0400

    changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-var word = 'Javahell';
+var word = 'javahell';
 var guessed = [];
 var guessesleft = 10
 
@@ -23,49 +23,13 @@ document.onkeyup = function(event) {
 
     $('#guesses').html(guessed);
     
+    };
+
+
+function myFunction() {
+    document.getElementById("Play Again").reset();
 };
-
-
-
-
-
-
-// //function intializeGame(){
-//     div_used = document.getElementById('used');
-//     div_puzzle = document.getElementById('puzzle');
-//     div_available = document.getElementById('available');
-
-// var buttons = function () {
-//     myButtons = document.getElementById('buttons');
-//     letters = document.createElement('ul');
-
-//     for (var i = 0; i < alphabet.length; i++) {
-//       letters.id = 'alphabet';
-//       list = document.createElement('li');
-//       list.id = 'letter';
-//       list.innerHTML = alphabet[i];
-//       check();
-//       myButtons.appendChild(letters);
-//       letters.appendChild(list);
-//     }
-
 
 
     
 
-
-// function chooseWord () {
-
-
-// for (var i = 0; i < words.length; i++){
-//     var ranNum= Math.floor(Math.random()*5);
-//     return ranNum;
-
-// function newGame() {
-//     var placeholders = '',
-//         frag = document.createDocumentFragment(),
-//         abc = ['A','B','C','D','E','F','G','H','I','J','K','L','M',
-//                'N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-// }
-
-//    
